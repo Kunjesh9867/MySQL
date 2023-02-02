f052e913e272866d8ec19aad3bf09e508514ff14
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2023-02-02 13:53:02 -0500

    File 10 added

---
+++ CodeWithMosh/File10(Febrauary2).sql
@@ -0,0 +1,6 @@
+-- STORED PROCEDURE is a database object contains a block of SQL code
+-- BENEFITS:
+-- Store and organize SQL
+-- Faster execution
+-- Data Security (can perform various operation likes inserting, updating, deleting)
+
