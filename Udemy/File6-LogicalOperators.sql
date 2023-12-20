0018a6ca95652ea93784abbc83b7707b5ebd4dec
Kunjesh9867 <kunjeshramani9867@gmail.com>
2023-12-20 01:46:19 -0500

    Constraints and ALTER added

--- Udemy/File6-LogicalOperators.sql
+++ Udemy/File6-LogicalOperators.sql
@@ -75,3 +75,43 @@ FROM books;
 -- IS NULL
 SELECT  * FROM books
 WHERE author_lname IS NULL;
+
+
+### Exercise ###
+SELECT 10!=10; -- 0
+SELECT 15 > 14 AND 99 -5 <= 94; -- 1
+SELECT 1 IN (5,3) OR 9 BETWEEN 8 AND 10;   -- 1
+
+SELECT * FROM books
+WHERE released_year < 1980;
+
+SELECT * FROM books
+WHERE author_lname IN ('Eggers', 'Chabon');
+
+SELECT * FROM books
+WHERE author_lname = 'Lahiri'  AND released_year > 2000;
+
+SELECT * FROM books
+WHERE pages BETWEEN 100 and 200;
+
+SELECT * FROM books
+WHERE author_lname LIKE 'C%' OR author_lname LIKE 'S%';
+
+
+SELECT title, author_lname,
+       CASE
+           WHEN title LIKE '%stories%' THEN 'Short Stories'
+           WHEN title LIKE '%Just Kids%' OR title LIKE 'A Heartbreaking Work' THEN 'Memoir'
+           ELSE 'Novel'
+       END AS TYPE
+FROM books;
+
+
+SELECT author_fname, author_lname,
+       CASE
+           WHEN COUNT(*) > 1 THEN CONCAT(COUNT(*), ' books')
+           ELSE CONCAT(COUNT(*), ' book')
+       END
+       FROM books
+GROUP BY author_fname, author_lname;
+
